d372ac8434b942c55a861cd3bc94f7db42e13acb
Mandar1jn <marijnkneppers@gmail.com>
2022-04-21 16:42:02 +0200

    Initial commit

---
+++ Engine/Patches/App.cs
@@ -0,0 +1,52 @@
+﻿using GSGE;
+using MonoMod;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+#pragma warning disable 1591
+#pragma warning disable CS0108, CS0626
+
+namespace Engine.Patches
+{
+    [MonoModPatch("global::GSGE.App")]
+    public class App : global::GSGE.App
+    {
+        public extern void orig_Initialize();
+
+        public void Initialize()
+        {
+
+            var dir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            string[] files = Directory.GetFiles(Path.Combine(dir,"mods"), "*.dll");
+
+            for(int i = 0; i < files.Length; i++)
+            {
+                Assembly asm = Assembly.LoadFrom(files[i]);
+
+                Type[] types = asm.GetTypes();
+
+                for (int j = 0; j < types.Length; j++) 
+                {
+                    Type type = types[j];
+                    if (!type.IsClass || type.IsAbstract || !type.IsSubclassOf(typeof(Mod)))
+                        continue;
+
+                    if (type.GetConstructor(Type.EmptyTypes)?.Invoke(new Object[0]) is Mod mod)
+                    {
+
+                    }
+                }
+            }
+
+            orig_Initialize();
+
+        }
+    }
+}
