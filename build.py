cf32a70a2ba248f5cc98106f3e9bf416c79fec36
Andre Issa <andrelouisissa@gmail.com>
2022-04-25 11:22:15 +1000

    add script to automate some steps

---
+++ build.py
@@ -0,0 +1,38 @@
+import os
+from pathlib import Path
+from shutil import copy, move
+from subprocess import run
+
+HERE = Path(os.getcwd())
+BUILD = ("dotnet", "build")
+PATCHED = HERE / "Patched"
+VANILLA = HERE / "Vanilla"
+BINARIES = HERE / "Engine/bin/Debug/net452"
+MONOMOD = BINARIES / "MonoMod.exe"
+HOOKGEN = BINARIES / "MonoMod.RuntimeDetour.HookGen.exe"
+GENARGS = ("--private",)
+BACKUP = PATCHED / "Engine.dll.bak"
+ENGINE = "Engine.dll"
+MMHOOK = "MMHOOK_Engine.dll"
+MONOMODDED = "MONOMODDED_"
+MONO = "Mono"
+
+if not PATCHED.exists():
+    os.mkdir(PATCHED)
+copy(VANILLA / ENGINE, BACKUP)
+    
+run(BUILD)
+run((MONOMOD, BINARIES / ENGINE))
+
+for path in os.scandir(BINARIES):
+    if path.is_file() and MONOMODDED in path.name:
+        move(path, BINARIES / path.name.replace(MONOMODDED,""))
+
+run((HOOKGEN,) + GENARGS + (BINARIES / ENGINE,), cwd=BINARIES)
+
+copy(BINARIES / MMHOOK, PATCHED / MMHOOK)
+copy(BINARIES / ENGINE, PATCHED / ENGINE)
+
+for path in os.scandir(BINARIES):
+    if path.is_file() and path.name[:4] == MONO:
+        copy(path, PATCHED / path.name)
