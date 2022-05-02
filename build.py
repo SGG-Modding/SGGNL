01b7a4ca6cb10a058e3ed4b4e80cfbf98eacd398
Andre Issa <andrelouisissa@gmail.com>
2022-05-02 21:26:30 +1000

    scripts support Bastion + don't patch patched bin

--- build.py
+++ build.py
@@ -11,27 +11,47 @@ BINARIES = HERE / "Engine/bin/Debug/net452"
 MONOMOD = BINARIES / "MonoMod.exe"
 HOOKGEN = BINARIES / "MonoMod.RuntimeDetour.HookGen.exe"
 GENARGS = ("--private",)
-BACKUP = PATCHED / "Engine.dll.bak"
-ENGINE = "Engine.dll"
+BACKUP = ".bak"
+ENGINE = "Engine"
+ENGINEDLL = "Engine.dll"
+ENGINEMM = "Engine.mm.dll"
 MMHOOK = "MMHOOK_Engine.dll"
 MONOMODDED = "MONOMODDED_"
 MONO = "Mono"
+EXE = ".exe"
+GAMEEXES = {"Bastion.exe"}
+GAMEDATA = ENGINEDLL
 
 if not PATCHED.exists():
     os.mkdir(PATCHED)
-copy(VANILLA / ENGINE, BACKUP)
+if m := tuple(filter(lambda x: (VANILLA / x).exists(), GAMEEXES)):
+    GAMEDATA = m[0]
+    
+BACKUPDATA = GAMEDATA + BACKUP
+VBACKUPDATA = VANILLA / BACKUPDATA
+VGAMEDATA = VANILLA / GAMEDATA
+if VBACKUPDATA.is_file():
+    copy(VBACKUPDATA, VGAMEDATA)
+copy(VGAMEDATA, PATCHED / BACKUPDATA)
     
 run(BUILD)
-run((MONOMOD, BINARIES / ENGINE))
+for path in os.scandir(VANILLA):
+    if path.is_file():
+        copy(path, BINARIES / path.name)
+if GAMEDATA in GAMEEXES:
+    game = GAMEDATA.replace(EXE, "")
+    copy(BINARIES / ENGINEMM, BINARIES / ENGINEMM.replace(ENGINE, game))
+    MMHOOK = MMHOOK.replace(ENGINE, game)
+run((MONOMOD, BINARIES / GAMEDATA))
 
 for path in os.scandir(BINARIES):
     if path.is_file() and MONOMODDED in path.name:
         move(path, BINARIES / path.name.replace(MONOMODDED,""))
 
-run((HOOKGEN,) + GENARGS + (BINARIES / ENGINE,), cwd=BINARIES)
+run((HOOKGEN,) + GENARGS + (BINARIES / GAMEDATA,), cwd=BINARIES)
 
 copy(BINARIES / MMHOOK, PATCHED / MMHOOK)
-copy(BINARIES / ENGINE, PATCHED / ENGINE)
+copy(BINARIES / GAMEDATA, PATCHED / GAMEDATA)
 
 for path in os.scandir(BINARIES):
     if path.is_file() and path.name[:4] == MONO:
